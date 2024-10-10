935ffd3d6890be9857ac03b86d30ac507d775fd8
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-10 23:57:23 +0300

    Basic annotations-add...Completed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" 
+this script defines a type annotated function
+that takes a float a and a float b as arguments
+and returns their sum as float
+"""
+
+def add(a: float, b: float) -> float:
+    """ 
+    function that takes a float a and a float b and returns their sum
+
+    returns: the sum of a and b
+    """
+    return a + b
